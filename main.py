a5aefc9b91ce9ca408ad3e1534fd3f8c7040bf12
mr-ikar <johnclark.off@gmail.com>
2025-11-10 00:05:25 +0200

    first steps

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/home/boss/LastResort/venv/bin/python3
+import arguably
+import os
+from datetime import date
+import string
+
+@arguably.command
+def lastresort(github):
+    """Last Resort - a right tool for coming doomsday."""
+    if(github):
+        print("cloning repos from 'repos.txt'...")
+        os.system(f"mkdir repos")
+        if(github):
+            with open("repos.txt", 'r') as file:
+                for repo in file:
+                    os.system(f"git clone {repo}")
+    else:
+        print("No argument given. Check arguments with -h/--help flag.")
+if __name__ == "__main__":
+    arguably.run()
